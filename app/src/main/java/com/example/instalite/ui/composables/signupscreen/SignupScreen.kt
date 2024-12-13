cb06c77f5bac16777c37d54beed8a672a6339261
Manaswini Kundeti <manaswini.kundeti@gmail.com>
2024-12-13 14:55:28 -0600

    Added basic layout for signup Screen

--- app/src/main/java/com/example/instalite/ui/composables/signupscreen/SignupScreen.kt
+++ app/src/main/java/com/example/instalite/ui/composables/signupscreen/SignupScreen.kt
@@ -1,9 +1,35 @@
 package com.example.instalite.ui.composables.signupscreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.navigation.NavController
+import com.example.instalite.R
 import com.example.instalite.viewmodel.InstaLiteViewModel
 
 @Composable
@@ -11,5 +37,68 @@ fun SignupScreen(
     navController: NavController,
     viewModel: InstaLiteViewModel
 ) {
-    Text(text = "SignUp Screen")
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .verticalScroll(
+                rememberScrollState()
+            ),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            val usernameState = remember {
+                mutableStateOf(TextFieldValue())
+            }
+            val emailState = remember {
+                mutableStateOf(TextFieldValue())
+            }
+            val passwordState = remember {
+                mutableStateOf(TextFieldValue())
+            }
+
+            Image(
+                painter = painterResource(id = R.drawable.ig_logo),
+                contentDescription = "Instagram Logo",
+                modifier = Modifier
+                    .width(250.dp)
+                    .padding(top = 16.dp)
+                    .padding(8.dp)
+            )
+            Text(
+                text = "Signup",
+                modifier = Modifier.padding(8.dp),
+                fontSize = 30.sp,
+                fontFamily = FontFamily.SansSerif
+            )
+            OutlinedTextField(
+                value = usernameState.value,
+                onValueChange = { usernameState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Username") }
+            )
+            OutlinedTextField(
+                value = emailState.value,
+                onValueChange = { emailState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Email") }
+            )
+            OutlinedTextField(
+                value = passwordState.value,
+                onValueChange = { passwordState.value = it },
+                modifier = Modifier.padding(8.dp),
+                label = { Text(text = "Password") },
+                visualTransformation = PasswordVisualTransformation()
+            )
+            Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(8.dp)) {
+                Text(text = "Signup")
+            }
+            Text(text = "Already a user? Go to login ->",
+                color = Color.Blue,
+                modifier = Modifier
+                    .padding(8.dp)
+                    .clickable { }
+            )
+
+        }
+    }
 }
